422135ab1e68eb124f062af25d0e78bca889cc73
孙海阳 <sunhaiyang02@meituan.com>
2020-05-08 11:37:41 +0800

    add: 添加最大子续和

---
+++ coding/dp/最大子序和.js
@@ -0,0 +1,33 @@
+/**
+ * @param {Array<Array<Number>>} map 输入地图数据
+ * @returns {Number} 返回所需的最小生命值
+ *
+ * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ * eg:
+ *   [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+ *   console.log(solve(map)); // => 6
+ * 解释: 连续子数组[4, -1, 2, 1] 的和最大，为 6。
+ */
+
+const maxSubArray = function (nums) {
+  let ans = nums[0];
+  let sum = 0;
+  for (const num of nums) {
+    if (sum > 0) {
+      sum += num;
+    } else {
+      sum = num;
+    }
+    ans = Math.max(ans, sum);
+  }
+  return ans;
+};
+
+
+// 思路：
+// 这道题用动态规划的思路并不难解决，比较难的是后文提出的用分治法求解，但由于其不是最优解法，所以先不列出来
+// 动态规划的是首先对数组进行遍历，当前最大连续子序列和为 sum，结果为 ans
+// 如果 sum > 0，则说明 sum 对结果有增益效果，则 sum 保留并加上当前遍历数字
+// 如果 sum <= 0，则说明 sum 对结果无增益效果，需要舍弃，则 sum 直接更新为当前遍历数字
+// 每次比较 sum 和 ans的大小，将最大值置为ans，遍历结束返回结果
+// 时间复杂度：O(n)O(n)
